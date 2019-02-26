f4c129b01dfce818152d2cad82eed61a503c8f68
Jonathan Keane <jkeane889@gmail.com>
2019-02-26 11:09:01 -0500

    Created script to read and write to another blank file in python

---
+++ ex17_a.py
@@ -0,0 +1,24 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}.")
+
+# we could do this on one line, how?
+with open(from_file) as fp: in_data = fp.read()
+
+print(f"The input file is {len(in_data)} bytes long.")
+
+print(">>>> in_data=", repr(in_data))
+
+print(f"Does the ouput file exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, "w")
+out_file.write(in_data)
+
+print("Alright, all done.")
+
+out_file.close()
