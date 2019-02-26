8a7cf2555f5133a7360bcbba8bdaf9bbbbaa78c2
Jonathan Keane <jkeane889@gmail.com>
2019-02-26 09:37:31 -0500

    Using argv in python to take user input and print to console

---
+++ ex13.py
@@ -0,0 +1,10 @@
+# Monty Python bridge scene
+
+from sys import argv
+
+script, knights_Name, knights_quest, knights_question = argv
+
+print("The script is called:", script)
+print("Your first variable is:", knights_Name)
+print("Your second variable is:", knights_quest)
+print("Your third variable is:", knights_question)
